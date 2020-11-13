08a8a85ff6b294b6bc86620ab4861c651885566a
Cleomir <cleomir.mg@hotmail.com>
2020-11-13 12:15:58 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import ClassComponent from "./components/Class";
-import Stateful from "./components/Stateful";
-import Events from "./components/Events";
-import StateManipulation from "./components/StateManipulation";
-import FormHanding from "./components/FormHandling";
-import Nested from "./components/Nested";
-import List from './components/List';
+// import ClassComponent from "./components/Class";
+// import Stateful from "./components/Stateful";
+// import Events from "./components/Events";
+// import StateManipulation from "./components/StateManipulation";
+// import FormHanding from "./components/FormHandling";
+// import Nested from "./components/Nested";
+// import List from './components/List';
 
 //export default ClassComponent;
 // export default Stateful;
 // export default FormHanding;
-export default Nested;
+// export default Nested;
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
