6968431c17d3275baee367e153f459c5e9ff359f
Cleomir <cleomir.mg@hotmail.com>
2020-12-15 12:06:45 +0200

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import "./index.css";
+import App from "./App";
+import simpleReducer from "./redux/simpleReducer";
 
-ReactDOM.render(  
+const store = createStore(simpleReducer);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
---
+++ src/redux/simpleReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  posts: [],
+};
+
+const simpleReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default simpleReducer;
